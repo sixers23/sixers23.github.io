41ee78d16954c5038da4a3172acecb951c5d96ef
Nicolas Misseri <63420582+sixers23@users.noreply.github.com>
2020-06-25 09:49:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,29 @@ function calcular() {
   var p = document.getElementById("peso").value;
   var a = document.getElementById("altura").value;
   const r = p / (a * a);
-  console.log(r);
 
   // Comparar el resultado para dar un diagnostico
 
-  var diagnostico = 0;
+  var diagnostico =
+    r < 17
+      ? "Desnutricion"
+      : 18 <= r && r <= 20
+      ? "Bajo peso"
+      : 21 <= r && r <= 25.9
+      ? "Normal"
+      : 26 <= r && r <= 30.9
+      ? "Sobrepeso"
+      : 31 <= r && r <= 35.9
+      ? "Obesidad"
+      : 36 <= r && r <= 40
+      ? "Obesidad marcada"
+      : "Obesidad morbida";
 
-  if (r < 17) {
-    diagnostico = "Desnutricion";
-  } else if (18 <= r && r <= 20) {
-    diagnostico = "Bajo peso";
-  } else if (21 <= r && r <= 25.9) {
-    diagnostico = "Normal";
-  } else if (26 <= r && r <= 30.9) {
-    diagnostico = "Sobrepeso";
-  } else if (31 <= r && r <= 35.9) {
-    diagnostico = "Obesidad";
-  } else if (36 <= r && r <= 40) {
-    diagnostico = "Obesidad marcada";
-  } else if (r > 40) {
-    diagnostico = "Obesidad morbida";
-  }
   document.getElementById("prompt").innerHTML = diagnostico;
 }
 
 var agenda = [];
-class dia {
+class Dia {
   constructor(f, d, a, m, c) {
     this.fecha = f;
     this.desayuno = d;
@@ -41,7 +38,7 @@ class dia {
 
 function agendar() {
   agenda.push(
-    new dia(
+    new Dia(
       document.getElementById("fecha").value,
       document.getElementById("breakfast").value,
       document.getElementById("lunch").value,
@@ -49,31 +46,23 @@ function agendar() {
       document.getElementById("dinner").value
     )
   );
-  console.log(agenda);
-  for (let index = 0; index < agenda.length; index++) {
-    document.getElementById("f").innerHTML = "Fecha: " + agenda[index].fecha;
-    document.getElementById("d").innerHTML =
-      "Desayuno: " + agenda[index].desayuno;
-    document.getElementById("a").innerHTML =
-      "Almuerzo: " + agenda[index].almuerzo;
-    document.getElementById("m").innerHTML =
-      "Merienda: " + agenda[index].merienda;
-    document.getElementById("c").innerHTML = "Cena: " + agenda[index].cena;
+
+  for (var dia of agenda) {
+    document.getElementById("f").innerHTML = "Fecha: " + dia.fecha;
+    document.getElementById("d").innerHTML = "Desayuno: " + dia.desayuno;
+    document.getElementById("a").innerHTML = "Almuerzo: " + dia.almuerzo;
+    document.getElementById("m").innerHTML = "Merienda: " + dia.merienda;
+    document.getElementById("c").innerHTML = "Cena: " + dia.cena;
   }
 }
 
 function mostrarAgenda() {
-  for (let index = 0; index < agenda.length; index++) {
-    for (let index = 0; index < agenda.length; index++) {
-      document.getElementById("f").innerHTML = "Fecha: " + agenda[index].fecha;
-      document.getElementById("d").innerHTML =
-        "Desayuno: " + agenda[index].desayuno;
-      document.getElementById("a").innerHTML =
-        "Almuerzo: " + agenda[index].almuerzo;
-      document.getElementById("m").innerHTML =
-        "Merienda: " + agenda[index].merienda;
-      document.getElementById("c").innerHTML = "Cena: " + agenda[index].cena;
-    }
+  for (var dia of agenda) {
+    document.getElementById("f").innerHTML = "Fecha: " + agenda.fecha;
+    document.getElementById("d").innerHTML = "Desayuno: " + dia.desayuno;
+    document.getElementById("a").innerHTML = "Almuerzo: " + dia.almuerzo;
+    document.getElementById("m").innerHTML = "Merienda: " + dia.merienda;
+    document.getElementById("c").innerHTML = "Cena: " + agenda.cena;
   }
 }
 
